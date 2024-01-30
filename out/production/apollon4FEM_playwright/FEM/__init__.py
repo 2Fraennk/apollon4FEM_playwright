c8c6b66b40d8ce7e7458ba8ae75b2fb0666e03c4
Frank Birkhofen <frank.birkhofen@telekom.de>
2024-01-30 14:37:57 +0100

    * remove out dir

--- out/production/apollon4FEM_playwright/FEM/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level='DEBUG', filename='../../logs/playwrightFEM.log', filemode='w',
-                    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
