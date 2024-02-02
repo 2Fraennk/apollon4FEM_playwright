7ec4bda5250c8c575d969ca9496761e23bf264ff
Frank Birkhofen <frank.birkhofen@telekom.de>
2024-02-02 14:49:34 +0100

    * append new loglines to existing logfiles

--- poetryPlaywright/src/activeMq/__init__.py
+++ poetryPlaywright/src/activeMq/__init__.py
@@ -3,5 +3,5 @@
 import logging
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level='DEBUG', filename='../logs/playwrightActiveMq.log', filemode='w',
+logging.basicConfig(level='DEBUG', filename='../logs/playwrightActiveMq.log', filemode='a',
                     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
