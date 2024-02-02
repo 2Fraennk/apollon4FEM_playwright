5b62ac96f4c46d42f990f182c3ab6924fd07acde
Frank Birkhofen <frank.birkhofen@telekom.de>
2024-02-02 15:05:50 +0100

    * tidy up for F.E.M.

--- poetryPlaywright/src/activeMq/__init__.py
+++
@@ -1,7 +0,0 @@
-# from .main import run
-
-import logging
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level='DEBUG', filename='../logs/playwrightActiveMq.log', filemode='w',
-                    format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
--- poetryPlaywright/src/confluence/__init__.py
+++
@@ -1,1 +0,0 @@
-from confluence.startPage import run
